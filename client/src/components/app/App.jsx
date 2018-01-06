afc80677a580051cceb36431baf0988bb0679c5a
Mindjet <pearl920@outlook.com>
2018-01-06 15:35:51 +0800

    Update: accomplish the architecture in client and implement simple demo

---
+++ client/src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                This is a app named 'Reask'
+            </div>
+        )
+    }
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Mindjet on 2018/1/6.
+ */
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+
+    entry: {
+        main: path.join(__dirname, 'src', 'index.jsx')
+    },
+    output: {
+        path: path.resolve(__dirname, 'build/'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['sass-loader', 'style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['build/'])
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    }
+
+};
+
+module.exports = config;
