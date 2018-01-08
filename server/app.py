60c25d8493929b64252ab8153626c909055a3110
Mindjet <pearl920@outlook.com>
2018-01-08 23:53:23 +0800

    Update: enable flask CORS

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from server.blueprints import blueprints
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 for blueprint in blueprints:
     app.register_blueprint(blueprint)
